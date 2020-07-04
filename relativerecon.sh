3a75606b9f2b1761afa422895e1bfb8000cb08f8
whitehat92 <ze_correia93@hotmail.com>
2020-07-04 16:02:15 +0100

    Update relativerecon.sh

--- relativerecon.sh
+++ relativerecon.sh
@@ -11,7 +11,7 @@ echo "running dirsearch"
 #/usr/bin/dirsearch $1 > $(echo $1 | sed 's/https:\/\///g' | sed 's/\//_/g')/directories.txt
 python3 /root/tools/dirsearch_new/dirsearch/dirsearch.py -u $1 -e html,htm,js,php,conf,dist,cnf,info,sql,asp,net,aspnet,dst,mysql,doc,pdf,xls,info,inf,txt,odt,php3,php4,php5,phtml,lib,test,inc,index,war,foo,rdz,zip,7z,.html,.htm,.js,.php,.conf,.dist,.cnf,.info,.sql,.asp,.net,.aspnet,.dst,.mysql,.doc,.pdf,.xls,.info,.inf,.txt,.odt,.php3,.php4,.php5,.phtml,.lib,.test,.inc,.index,.war,.foo,.rdz,.zip,.7z -t 10 -F -b -r --plain-text-report=$(echo $1 | sed 's/https:\/\///g' | sed 's/\//_/g')/directories.txt
 if grep -i "server: " $(echo $1 | sed 's/https:\/\///g' | sed 's/\//_/g')/curl.txt; then
-grep -i "server: " $(echo $1 | sed 's/https:\/\///g' | sed 's/\//_/g')/curl.txt | awk -F ":" '{print $2}') > $(echo $1 | sed 's/https:\/\///g' | sed 's/\//_/g')/server.txt
+grep -i "server: " $(echo $1 | sed 's/https:\/\///g' | sed 's/\//_/g' )/curl.txt | awk -F ":" '{print $2}' > $(echo $1 | sed 's/https:\/\///g' | sed 's/\//_/g')/server.txt
 fi
 
 #bash relativerecon.sh example.com/something
